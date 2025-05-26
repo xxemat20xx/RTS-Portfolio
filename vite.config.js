29be4bdbff68655a8d112020d6bd18fda5c0ec2a
xxemat20xx <62041802+xxemat20xx@users.noreply.github.com>
2025-05-27 02:51:08 +0800

    commit fix tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/RTS-Portfolio/',
-  plugins: [react(), tailwindcss()],
-});
+  plugins: [
+    react(), //react plugin
+    tailwindcss(), // tailwind plugin
+  ],
+})
