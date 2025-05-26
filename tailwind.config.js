d2bcebeea886a4b9421f74857fc69a08051bc789
xxemat20xx <62041802+xxemat20xx@users.noreply.github.com>
2025-05-27 02:39:29 +0800

    commit changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
-import tailwindcss from 'tailwindcss';
-import react from '@vitejs/plugin-react';
-export default defineConfig({
-  plugins: [react(), tailwindcss],
-});
+// tailwind.config.js
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
