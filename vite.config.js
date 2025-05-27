2f44f3fd0c87f377e1f90af731a5d587f49242ae
xxemat20xx <62041802+xxemat20xx@users.noreply.github.com>
2025-05-28 01:23:19 +0800

    fix css issue

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/postcss'
 import autoprefixer from 'autoprefixer'
-
+const repoName = 'RTS-Portfolio'
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react()],
   css: {
     postcss: {
