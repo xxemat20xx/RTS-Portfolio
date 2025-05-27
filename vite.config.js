187f9dcc510d5bfb66c5bdef39f3357aa9a2f01d
xxemat20xx <62041802+xxemat20xx@users.noreply.github.com>
2025-05-28 01:08:45 +0800

    fix to css issue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), //react plugin
-    tailwindcss(), // tailwind plugin
-  ],
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer(),
+      ],
+    },
+  },
+  server: {
+    port: 8080,
+  },
 })
